a5a0d1e8054427e4f22a7c038f84921cb9ccad0d
Aleksandras Nelkinas <a.nelkinas@gmail.com>
2019-04-12 12:01:25 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-static-plugin-scss-modules
+
+An opinionated React-Static plugin that enables CSS Modules with SCSS syntax and PostCSS processing.
---
+++ node.api.js
@@ -0,0 +1,63 @@
+import { getLocalIdent } from 'css-loader/dist/utils';
+import ExtractCssChunks from 'extract-css-chunks-webpack-plugin';
+
+export default () => {
+  return {
+    webpack(config, { stage }) {
+      const styleLoader = {
+        loader: require.resolve('style-loader'),
+      };
+      const cssLoader = {
+        loader: require.resolve('css-loader'),
+        options: {
+          modules: true,
+          importLoaders: 2,
+          localIdentName: '[name]-[local]_[hash:base64:4]',
+          getLocalIdent: (context, localIdentName, localName, options) =>
+            context.resourcePath.includes('/node_modules/')
+              ? localName
+              : getLocalIdent(context, localIdentName, localName, options),
+        },
+      };
+      const postcssLoader = {
+        loader: require.resolve('postcss-loader'),
+        options: {
+          plugins: [
+            require('postcss-flexbugs-fixes'),
+            require('autoprefixer')({
+              flexbox: 'no-2009',
+            }),
+          ],
+        },
+      };
+      const sassLoader = {
+        loader: require.resolve('sass-loader'),
+      };
+
+      config.module.rules[0].oneOf.unshift({
+        test: /\.scss$/,
+        use: {
+          dev: [styleLoader, cssLoader, postcssLoader, sassLoader],
+          node: [
+            extendOptions(cssLoader, { exportOnlyLocals: true }),
+            postcssLoader,
+            sassLoader,
+          ],
+          prod: [ExtractCssChunks.loader, cssLoader, postcssLoader, sassLoader],
+        }[stage],
+      });
+
+      return config;
+    },
+  };
+
+  function extendOptions(loader, options) {
+    return {
+      ...loader,
+      options: {
+        ...loader.options,
+        ...options,
+      },
+    };
+  }
+};
